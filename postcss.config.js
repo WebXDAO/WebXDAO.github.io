443895cad39d9a3f9f40c7062867ea4a91bed1eb
Maxime Kubik <mkubik@tftlabs.com>
2021-09-27 17:54:52 +0200

    🔧 config: add css + tailwind config

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    require(`tailwindcss`)(`./tailwind.config.js`),
+    require(`autoprefixer`),
+    ...(process.env.NODE_ENV === "production"
+      ? [
+          require(`cssnano`)({
+            preset: "default",
+          }),
+        ]
+      : []),
+  ],
+};
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+   module.exports = {
+  purge: {
+    mode: "all",
+    content: ["./**/*.html"],
+    options: {
+      whitelist: [],
+    },
+  },
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {},
+    },
+  },
+  variants: {},
+  plugins: [require("@tailwindcss/typography")],
+};
