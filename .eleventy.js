41458ebd5e6a771e7d5c388251e41c398898be73
Maxime Kubik <mkubik@tftlabs.com>
2021-09-28 14:03:51 +0200

    💄 add favicon (woops)
    Added the favicon in the build folder (check .eleventy.js)

--- .eleventy.js
+++ .eleventy.js
@@ -38,7 +38,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/static/img");
 
   // Copy favicon to route of /_site
-  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("./src/static/img/favicon.ico");
 
   // Minify HTML
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
