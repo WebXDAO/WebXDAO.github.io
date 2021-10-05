b56fe038637ccb668ddd0ada56eef27ae03ea5c1
Maxime TFT <mkubik@tftlabs.com>
2021-10-05 16:45:48 +0200

    fix: empty lines

--- src/_data/devto.js
+++ src/_data/devto.js
@@ -1,13 +1,13 @@
 const fetch = require("node-fetch");
 const Cache = require("@11ty/eleventy-cache-assets");
 
-module.exports = async function() {
+module.exports = async function () {
   let url = "https://dev.to/api/articles?username=web3community";
 
   const response = await Cache(url, {
-		duration: "4h", // save for 4 hours
-		type: "json" // we’ll parse JSON for you
-	});
+    duration: "4h", // save for 4 hours
+    type: "json" // we’ll parse JSON for you
+  });
 
   // uncomment to see json coming from dev.to api
   console.log("Fetching new dev.to articles...", response)
