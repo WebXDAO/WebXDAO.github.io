1db2d9610bfb3f62da6c310fc8955d0c3ce82568
Eui Hyun Kim <hyomithetech@gmail.com>
2022-02-08 22:33:06 -0800

    Work inprocess

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+    colors: {
+      black: "#19181C",
+      gray: "#7F7D83",
+    },
     extend: {
-      fontFamily: { poppins: ['Poppins'] }
-    }
+      fontFamily: { poppins: ["Poppins"] },
+    },
   },
-  plugins: ['@tailwindcss/typography']
-}
+  plugins: ["@tailwindcss/typography"],
+};
