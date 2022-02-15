f8e7869c8c07066baa06af5c6a324a4b86caa24a
Eui Hyun Kim <hyomithetech@gmail.com>
2022-02-14 23:41:53 -0800

    Change colors, add new logo and buttons

--- components/Global/Navbar.jsx
+++ components/Global/Navbar.jsx
@@ -10,7 +10,7 @@ const NavBarItem = ({ item, pathname, classprops }) => (
     <li
       className={`${
         pathname === item.url && "font-bold"
-      } ${classprops} cursor-pointer text-ml inline-block text-blue-800 no-underline hover:text-indigo-500 p-2 xl:px-4 items-center`}
+      } ${classprops} cursor-pointer text-lg inline-block text-gray-500 no-underline hover:text-indigo-500 p-2 xl:px-4 items-center`}
     >
       {item.title}
     </li>
@@ -31,66 +31,72 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="sticky w-full z-30 top-0">
-      <div className="w-full bg-black container flex items-center justify-between mt-0 py-2 text-gray">
-        <div className="pl-4 flex items-center cursor-pointer">
-          <Link
-            className="no-underline hover:no-underline font-bold text-2xl lg:text-2xl"
-            href="/"
-            passHref
-          >
-            <Image
-              layout="fixed"
-              width={150}
-              height={80}
-              src="/logo.png"
-              alt="WebXDAO"
-            />
-          </Link>
-        </div>
-        <ul className="lg:flex hidden list-none flex-row justify-end items-center ml-auto">
-          {navItems.map((item, index) => (
-            <NavBarItem
-              key={item.title + index}
-              item={item}
-              pathname={router.asPath}
-            />
-          ))}
-        </ul>
-        <div className="flex relative p-2">
-          {!toggleMenu && (
-            <HiMenuAlt4
-              fontSize={28}
-              className="lg:hidden cursor-pointer text-blue-600"
-              onClick={() => setToggleMenu(true)}
-            />
-          )}
-          {toggleMenu && (
-            <AiOutlineClose
-              fontSize={28}
-              className="lg:hidden cursor-pointer text-blue-600"
-              onClick={() => setToggleMenu(false)}
-            />
-          )}
-          {toggleMenu && (
-            <ul
-              className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl lg:hidden list-none
+    <nav className="sticky w-full z-30 top-0 bg-black flex text-gray justify-between items-center">
+      <div className="pl-4 m-2 flex items-center cursor-pointer">
+        <Link
+          className="no-underline hover:no-underline font-bold text-2xl lg:text-2xl"
+          href="/"
+          passHref
+        >
+          <Image
+            layout="fixed"
+            width={73}
+            height={73}
+            src="/new logo.svg"
+            alt="WebXDAO"
+          />
+        </Link>
+      </div>
+      <ul className="lg:flex hidden list-none flex-row items-center">
+        {navItems.map((item, index) => (
+          <NavBarItem
+            key={item.title + index}
+            item={item}
+            pathname={router.asPath}
+          />
+        ))}
+      </ul>
+      <div className="flex relative p-2">
+        {!toggleMenu && (
+          <HiMenuAlt4
+            fontSize={28}
+            className="lg:hidden cursor-pointer text-blue-600"
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <AiOutlineClose
+            fontSize={28}
+            className="lg:hidden cursor-pointer text-blue-600"
+            onClick={() => setToggleMenu(false)}
+          />
+        )}
+        {toggleMenu && (
+          <ul
+            className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl lg:hidden list-none
 flex flex-col justify-start items-end rounded-md blue-glassmorphism"
-            >
-              <li className="text-xl w-full my-2 text-blue-600">
-                <AiOutlineClose onClick={() => setToggleMenu(false)} />
-              </li>
-              {navItems.map((item, index) => (
-                <NavBarItem
-                  key={item + index}
-                  item={item}
-                  pathname={router.asPath}
-                  classprops={"my-2 text-lg !text-black font-bold"}
-                />
-              ))}
-            </ul>
-          )}
-        </div>
+          >
+            <li className="text-xl w-full my-2 text-blue-600">
+              <AiOutlineClose onClick={() => setToggleMenu(false)} />
+            </li>
+            {navItems.map((item, index) => (
+              <NavBarItem
+                key={item + index}
+                item={item}
+                pathname={router.asPath}
+                classprops={"my-2 text-lg !text-black font-bold"}
+              />
+            ))}
+          </ul>
+        )}
+      </div>
+      <div className="text-white">
+        <button className="gradient-horizontal p-3 rounded-xl font-bold">
+          Join community
+        </button>
+        <button className="gradient-horizontal p-3 rounded-xl font-bold ml-6">
+          Partner with us
+        </button>
       </div>
       <hr className="border-b border-gray-100 opacity-25 my-0 py-0" />
     </nav>
