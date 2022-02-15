f4b2965598e3efa2c4ed2cf80ff944181a571f64
Eui Hyun Kim <hyomithetech@gmail.com>
2022-02-14 23:22:36 -0800

    Add Ubunto Mono font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      black: "#19181C",
-      gray: "#7F7D83",
-    },
     extend: {
-      fontFamily: { poppins: ["Poppins"] },
+      fontFamily: {
+        poppins: ["Poppins"],
+        ubuntoMono: ["Ubuntu Mono", "monospace"],
+      },
     },
   },
   plugins: ["@tailwindcss/typography"],
