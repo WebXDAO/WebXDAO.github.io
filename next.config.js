672cd67bc1ff2266f1827eaf8e8f4f5ecf70486a
Maxime TFT <mkubik@tftlabs.com>
2022-09-13 17:32:00 +0200

    [🧪] bump nextjs to v12 + update tailwind config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' }
-    }
-  },
+  // exportPathMap: function () {
+  //   return {
+  //     '/': { page: '/' }
+  //   }
+  // },
   reactStrictMode: true,
   images: {
-    domains: ['res.cloudinary.com', 'github.com']
-  }
-}
+    domains: ['res.cloudinary.com', 'github.com'],
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
+    './context/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
-      fontFamily: { poppins: ['Poppins'] }
-    }
+      fontFamily: {
+        poppins: ['Poppins'],
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'cyber-webx': '#5f074d',
+      },
+    },
   },
-  plugins: ['@tailwindcss/typography']
-}
+  plugins: ['@tailwindcss/forms', '@tailwindcss/typography'],
+};
