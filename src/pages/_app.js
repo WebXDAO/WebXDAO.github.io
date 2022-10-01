3fb0497878d28c7478bd4cd74500219625ba0e88
Ishaan Bedi <bediishaan6@gmail.com>
2022-10-01 14:25:08 +0530

    Add SEO Optimization (#204)
    * Add SEO Optimization
    
    * Fix Suggested Changes against SEO Optimization

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,12 @@
 import WebXLayout from '../layouts/WebXLayout';
 import '../styles/globals.css';
+import SEO from '../../next-seo.config';
+import { DefaultSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <DefaultSeo {...SEO} />
       <WebXLayout>
         <Component {...pageProps} />
       </WebXLayout>
