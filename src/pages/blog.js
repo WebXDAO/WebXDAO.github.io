a8e88000058e94cd4ed69508c4878386ee382cdc
anuran12 <anuranarts@gmail.com>
2022-10-01 23:58:07 +0530

    Improve

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -37,11 +37,11 @@ const blogs = ({ data }) => {
 
             {data.map((curElem) => {
               return (
-                <div className='group z-2 relative w-min h-min bg-white bg-opacity-5 m-10 shadow-2xl rounded-2xl flex justify-conten items-center backdrop-filter backdrop-blur-md'>
+                <div className='group z-2 relative w-min h-min bg-white bg-opacity-5 m-10 shadow-2xl rounded-2xl flex justify-conten items-center backdrop-filter backdrop-blur-md' key={curElem.id}>
                   <div className='relative xl:w-[35rem] xs:w-[17rem] h-80 flex justify-center items-center flex-col opacity-70  hover:opacity-100'>
 
                     <div className='relative w-4/5 rounded-2xl truncate border-8 border-solid border-black/25 group-hover:-translate-y-6 duration-500'>
-                      <img src={curElem.cover_image} className='relative top-0 left-0 w-full h-full object-cover ' />
+                      <img src={curElem.cover_image} alt='blog image' className='relative top-0 left-0 w-full h-full object-cover ' />
                     </div>
                     <div className='relative justify-center items-center flex-wrap mx-0 my-0 text-center'>
                       <h3 className='group-hover:-translate-y-6 duration-500 text-white uppercase font-medium text-sm text-center tracking-wider mt-5 mr-2.5 ml-2.5 mb-2.5 leading-4 '>
@@ -50,7 +50,7 @@ const blogs = ({ data }) => {
                         <spam className='text-xs font-light lowercase'>
                           {curElem.description}
                         </spam>
-                        <a href={curElem.url} target="_blank" className='rounded-md bg-green-500 pl-4 pr-4 text-base font-normal mb-0 mt-0 m-auto'>
+                        <a href={curElem.url} target="_blank" rel='noopener noreferrer' className='rounded-md bg-green-500 pl-4 pr-4 text-base font-normal mb-0 mt-0 m-auto'>
                           <b>Read</b>
                         </a>
                       </h3>
