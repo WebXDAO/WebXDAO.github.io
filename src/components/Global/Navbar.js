6c39baac1eaa085a1fa25e48d8f619b25bf51752
Ahmed Silat <105588821+ahmedsilat44@users.noreply.github.com>
2022-10-01 20:45:33 +0500

    Added "import Link from 'next/link';"

--- src/components/Global/Navbar.js
+++ src/components/Global/Navbar.js
@@ -1,5 +1,5 @@
-import { Fragment } from "react";
 import Link from 'next/link';
+import { Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { SiGithubsponsors } from "react-icons/si";
