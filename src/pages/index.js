6eea81c1a1bfbac59c34d7f0d6174f5fba73171c
Anurag Sharma <anuragsharma011011@gmail.com>
2022-10-05 19:33:37 +0530

    Update src/pages/index.js
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ export default function Home({ articles }) {
         <meta name="description" content="We are an opensource community working around the future of the web. Learn blockchain technology together. "/>
         <meta name="keywords" content="WebXDAO,Web,Web3,Blockchain,OpenSource,DAO"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="author" content="WebXDAO community">
       </Head>
       <div className="flex flex-col h-screen">
         <Hero />
