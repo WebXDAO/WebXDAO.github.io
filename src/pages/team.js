cc6aa54ce43254584cee7483927eeb9f92899ee0
vinzvinci <vincent.villafuerte1@gmail.com>
2023-02-20 12:30:26 +0800

    feat: added maintainers and moderators team

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
+import Link from 'next/link'
 
-const people = [
+const core = [
   {
     name: 'Vincent Villafuerte',
     role: 'WebX Core',
@@ -39,7 +40,37 @@ const people = [
   },
 ]
 
-export default function Example() {
+const mod = [
+  {
+    name: 'David Leal',
+    role: 'Mods / Maintainer',
+    imageUrl:
+      'https://github.com/Panquesito7.png',
+    githubUrl: 'https://github.com/Panquesito7',
+    twitterUrl: 'https://twitter.com/David_Leal_7',
+    linkedinUrl: '#',
+  },
+  {
+    name: 'Krish Gupta',
+    role: 'Mods / Maintainer',
+    imageUrl:
+      'https://github.com/krshkun.png',
+    githubUrl: 'https://github.com/krshkun',
+    twitterUrl: 'https://twitter.com/krshkun',
+    linkedinUrl: 'https://linkedin.com/in/krshkun',
+  },
+  {
+    name: 'Abhinav MV',
+    role: 'Maintainer',
+    imageUrl:
+      'https://github.com/AbhinavMV.png',
+    githubUrl: 'https://github.com/AbhinavMV',
+    twitterUrl: 'https://twitter.com/abhinav_m_v',
+    linkedinUrl: 'https://www.linkedin.com/in/abhinav-mv-b216b8160/',
+  },
+]
+
+export default function Teams() {
   return (
     <div className="py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 text-center lg:px-8">
@@ -53,7 +84,7 @@ export default function Example() {
           role="list"
           className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-6 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:gap-8"
         >
-          {people.map((person) => (
+          {core.map((person) => (
             <li key={person.name} className="rounded-2xl bg-gray-800 py-10 px-8">
               <img className="mx-auto h-48 w-48 rounded-full md:h-56 md:w-56" src={person.imageUrl} alt="" />
               <h3 className="mt-6 text-base font-semibold leading-7 tracking-tight text-white">{person.name}</h3>
@@ -92,6 +123,59 @@ export default function Example() {
           ))}
         </ul>
       </div>
+      {/* Maintainers and Moderators Section */}
+      <div className="py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 text-center lg:px-8">
+        <div className="mx-auto max-w-2xl">
+          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Maintainers and Moderators Team</h2>
+          <p className="mt-4 text-lg leading-8 text-gray-400">
+          Our maintainers and moderators play a crucial role in our community projects by keeping our codebase in good shape and making sure our community stays active and friendly.
+          </p>
+        </div>
+        <ul
+          role="list"
+          className="mx-auto mt-20 grid max-w-2xl grid-cols-2 gap-y-16 gap-x-8 text-center sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none lg:grid-cols-5 xl:grid-cols-6"
+        >
+          {mod.map((person) => (
+            <li key={person.name}>
+              <img className="mx-auto h-24 w-24 rounded-full" src={person.imageUrl} alt="" />
+              <h3 className="mt-6 text-base font-semibold leading-7 tracking-tight text-white">{person.name}</h3>
+              <p className="text-sm leading-6 text-gray-400">{person.role}</p>
+              <ul role="list" className="mt-6 flex justify-center gap-x-6">
+                <li>
+                  <a href={person.githubUrl} className="text-gray-400 hover:text-gray-300">
+                    <span className="sr-only">GitHub</span>
+                    <svg className="h-5 w-5" viewBox="0 0 496 512">
+                      <path fillRule="evenodd" fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/>
+                    </svg>
+                  </a>
+                </li>
+                <li>
+                  <a href={person.twitterUrl} className="text-gray-400 hover:text-gray-300">
+                    <span className="sr-only">Twitter</span>
+                    <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                      <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+                    </svg>
+                  </a>
+                </li>
+                <li>
+                  <a href={person.linkedinUrl} className="text-gray-400 hover:text-gray-300">
+                    <span className="sr-only">LinkedIn</span>
+                    <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                      <path
+                        fillRule="evenodd"
+                        d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </a>
+                </li>
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
     </div>
   )
 }
