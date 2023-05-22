0764433a53f6a59bf8c559539d8ecc1dd4b4e388
Tushar Saini <s2sharpit@gmail.com>
2023-05-22 22:50:20 +0530

    adding output in next.config.mjs
    Signed-off-by: Tushar Saini <s2sharpit@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
+  output: "export",
   images: {
     domains: ["res.cloudinary.com", "github.com"],
   },
-}
+};
 
 export default nextConfig
