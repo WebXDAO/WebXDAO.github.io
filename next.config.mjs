934c87cc54e8a7e829e1f131ba80fce6c52f595b
Mustafiz Kaifee Mumtaz <mustafiz.mumtaz@medibuddy.in>
2023-05-23 21:11:47 +0530

    fix:next confix file

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
   images: {
     domains: ["res.cloudinary.com", "github.com"],
-    unoptimized: true
-  }
-};
+  },
+}
 
 export default nextConfig
