97f2427785a0cd802aa4f8e02c84a0ee91625996
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-05-23 13:04:57 +0000

    fix: error undefined related links

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -14,7 +14,7 @@ export function SiteHeader() {
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
             <Link
-              href={siteConfig.links.github}
+              href={siteConfig.links.webxdao_gh}
               target="_blank"
               rel="noreferrer"
             >
@@ -29,7 +29,7 @@ export function SiteHeader() {
               </div>
             </Link>
             <Link
-              href={siteConfig.links.twitter}
+              href={siteConfig.links.webxdao_twitter}
               target="_blank"
               rel="noreferrer"
             >
