6e096b3d1cc8b7267e9cbd70f4a99ae4265ddce7
Soulpurr <92094765+Soulpurr@users.noreply.github.com>
2023-05-25 07:35:14 +0530

    #347:Add a feature to hover on card items

--- components/containers/Partners.tsx
+++ components/containers/Partners.tsx
@@ -34,7 +34,7 @@ const Partners: FC<PartnersProps> = ({ }) => {
 
             <div className="mt-10 flex flex-wrap justify-center gap-24">
                 {data.map(({ img, para, url }, index) => (
-                    <div key={index} className="flex max-w-xs flex-col rounded-lg bg-white shadow-md shadow-purple-300">
+                    <div key={index} className="cursor-pointer transition-all duration-200 ease-in transform sm:hover:scale-105 hover:z-50 flex max-w-xs flex-col rounded-lg bg-white shadow-md shadow-purple-300">
                         <Image className="w-full" src={img} width={400} height={250} alt="" />
                         <div className="grow px-6 py-4">
                             <p className="text-justify text-sm text-gray-700">{para}</p>
@@ -43,10 +43,12 @@ const Partners: FC<PartnersProps> = ({ }) => {
                             <Link
                                 href={url}
                                 target="_blank"
-                                className="w-fit-content font-inter text-14.0418 mb-0 mr-auto flex items-center gap-6 rounded border-2 border-solid border-black px-4 py-1 font-semibold text-black group transition ease-in hover:bg-indigo-800 hover:border-indigo-800"
+                                className="m-auto w-fit-content font-inter text-14.0418 mb-0 mr-auto flex items-center gap-6 rounded border-2 border-solid border-black px-4 py-1 font-semibold text-black group transition ease-in hover:bg-indigo-800 hover:border-indigo-800"
                             >
                                 <Image src="/Arrow1.png" alt="arrow" width={15} height={18} className="my-auto flex items-center group-hover:invert transition duration-500 ease-in filter " />
-                                <span className="group-hover:invert transition duration-500 ease-in">Read More</span>
+                              
+                                <span className=" group-hover:invert transition duration-500 ease-in">Read More</span>
+                               
                             </Link>
                         </div>
                     </div>
