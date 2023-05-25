046e8bea463a0fa6e147b6f653064366ec0456a0
Soulpurr <92094765+Soulpurr@users.noreply.github.com>
2023-05-25 17:14:29 +0530

    #348-mobile navbar fixed

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -12,7 +12,7 @@ export function SiteHeader() {
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
-          <nav className="flex items-center space-x-1">
+          <nav className="hidden md:flex items-center space-x-1">
             <Link
               href={siteConfig.links.webxdao_gh}
               target="_blank"
