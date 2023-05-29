affbe19ce2c49da5437e74ca545f5872848ae75e
Manisha Swain <manishaswain2003@gmail.com>
2023-05-29 16:19:53 +0530

    added the code fro loader and review!

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,14 +1,43 @@
-import { SiteHeader } from "@/components/site-header"
+"use client ;";
+import { SiteHeader } from "@/components/site-header";
+
+import React, { useEffect, useState } from "react";
+
+import { Grid } from "react-loader-spinner";
 
 interface LayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export function Layout({ children }: LayoutProps) {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
   return (
     <>
-      <SiteHeader />
-      <main>{children}</main>
+      {loading ? (
+        <div className="flex h-screen items-center justify-center">
+          <Grid
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="grid-loading"
+            radius="12.5"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        </div>
+      ) : (
+        <div>
+          <SiteHeader />
+          <main>{children}</main>
+        </div>
+      )}
     </>
-  )
+  );
 }
