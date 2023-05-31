ef14c9544e372824b3ec79fadacfa37ab3990656
Maxime Kubik <mkubik@tftlabs.com>
2023-05-31 16:57:11 +0200

    fix: button with incorrect revert class, now fixed using dark/white variants

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Icons } from "./icons";
+import { Button, buttonVariants } from "./ui/button";
 
 const ProjectCard = ({
   full_name,
@@ -11,7 +12,7 @@ const ProjectCard = ({
   description: string;
 }) => {
   return (
-    <div className="bg-white-900/60 dark:border-white-700 flex h-full w-full flex-col justify-between rounded-lg border p-5 shadow duration-200 delay-100">
+    <div className="bg-white-900/60 dark:border-white-700 flex h-full w-full flex-col justify-between rounded-lg border p-5 shadow">
       <div>
         <div className="flex flex-row">
           {/*<img src={owner?.avatar_url} className="rounded h-40 w-40"/>*/}
@@ -20,20 +21,15 @@ const ProjectCard = ({
           </p>
         </div>
         <p className="dark:text-white-500 mt-3 text-xs text-gray-500">{description}</p>
-        <div className="flex px-6 py-4">
+        <div className="flex items-center justify-center px-2 py-4">
           <Link
             href={url}
             target="_blank"
-            className="w-fit-content font-inter text-14.0418 group m-auto mb-0 flex items-center gap-6 rounded border-2 border-black px-4 py-1 font-semibold text-black transition ease-in hover:border-indigo-800 hover:bg-indigo-800 hover:text-white dark:border-white dark:text-white"
+            rel="noreferrer"
+            className={buttonVariants({ size: "lg" })}
           >
-            <Icons.ArrowUpRight
-              width={20}
-              height={18}
-              className="dark:color-white my-auto flex items-center transition duration-500 ease-in group-hover:invert"
-            />
-
-            <span className=" transition duration-500 ease-in hover:invert">Read More</span>
-          </Link>
+            Read more
+          </Link> 
         </div>
       </div>
     </div>
