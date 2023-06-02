226c0de0d0a7097d287e5363c62bf1eb7084f872
Tushar Saini <s2sharpit@gmail.com>
2023-06-02 13:09:29 +0530

    adding gap between navbar and main content

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -8,7 +8,7 @@ import { ThemeToggle } from "@/components/theme-toggle"
 
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background">
+    <header className="sticky top-0 z-40 mb-8 w-full border-b bg-background">
       <div className="container flex h-20 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
       </div>
