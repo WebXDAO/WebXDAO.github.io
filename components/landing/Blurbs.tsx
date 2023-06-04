70b5d8bee0fd2751f74dc4a107fca09b8828265d
parthpathakpp1 <85991449+parthpathakpp1@users.noreply.github.com>
2023-06-04 22:54:01 +0530

    Removed Cursor Pointer

--- components/landing/Blurbs.tsx
+++ components/landing/Blurbs.tsx
@@ -5,7 +5,7 @@ export default function Blurbs() {
       <section className=" flex justify-center px-4 py-0 md:px-10">
         <div className="my-12 flex flex-col md:flex-row md:gap-8">
           <div className="flex flex-col items-center justify-center gap-3 text-center">
-            <div className="border-l-3 flex justify-center rounded-lg cursor-pointer border-white bg-white p-8 shadow-md shadow-purple-300 hover:scale-105 hover:-translate-y-2 transition-all duration-300">
+            <div className="border-l-3 flex justify-center rounded-lg border-white bg-white p-8 shadow-md shadow-purple-300 transition-all duration-300 hover:-translate-y-2 hover:scale-105">
               <Icons.collaborate className="h-15 w-15 mr-1" />
             </div>
             <div className="text-lg font-bold">Collaborate</div>
@@ -15,7 +15,7 @@ export default function Blurbs() {
           </div>
 
           <div className="mt-8 flex flex-col items-center justify-center gap-3 text-center md:mt-0">
-            <div className="border-l-3 flex justify-center rounded-lg cursor-pointer border-white bg-white p-8 shadow-md shadow-purple-300 hover:scale-105 hover:-translate-y-2 transition-all duration-300">
+            <div className="border-l-3 flex justify-center rounded-lg border-white bg-white p-8 shadow-md shadow-purple-300 transition-all duration-300 hover:-translate-y-2 hover:scale-105">
               <Icons.blockchain className="h-15 w-15 mr-1" />
             </div>
             <div className="text-lg font-bold">Blockchain</div>
@@ -26,7 +26,7 @@ export default function Blurbs() {
           </div>
 
           <div className="mt-8 flex flex-col items-center justify-center gap-3 text-center md:mt-0">
-            <div className="border-l-3 flex justify-center rounded-lg cursor-pointer border-white bg-white p-8 shadow-md shadow-purple-300 hover:scale-105 hover:-translate-y-2 transition-all duration-300">
+            <div className="border-l-3 flex justify-center rounded-lg border-white bg-white p-8 shadow-md shadow-purple-300 transition-all duration-300 hover:-translate-y-2 hover:scale-105">
               <Icons.decentralization className="h-15 w-15 mr-1" />
             </div>
             <div className="text-lg font-bold">Decentralization</div>
