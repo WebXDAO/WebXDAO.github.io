23bdd17ea9c38c8af284647109a34923e570afa9
Tushar Saini <s2sharpit@gmail.com>
2023-06-26 13:17:42 +0530

    next lint auto-changes for ESLint

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -22,7 +22,7 @@ export default async function Projects() {
       <Head>
         <title>Projects | WebXDAO</title>
       </Head>
-      <section className="py-8 overflow-hidden w-full">
+      <section className="w-full overflow-hidden py-8">
         <div className="container mx-auto flex max-w-5xl flex-row flex-wrap justify-around lg:flex-nowrap">
           <h1
             className={cn(
